7e29efae65830995520e881caa90b6abac61c96c
plajiw <pabloribeiro.cont@gmail.com>
2026-02-18 23:15:04 -0300

    ajustar projeto

--- estruturas-de-dados/ponteiros/exercicios/ponteiros-1.c
+++ estruturas-de-dados/ponteiros/exercicios/ponteiros-1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int vetor[10];
-    int *p = vetor;
+    int* p = vetor;
 
     for (int i = 0; i < 10; i++)
-    {   
+    {
         scanf("%d", p);
         printf("Endereco: %p e Valor: %d\n", p, *p);
         p++;
